4c2660c210a3e7441be30a578b3db5ea7f569700
h@ckrul3r <hackruler5770@gmail.com>
2023-12-02 23:20:59 +0530

    Update cidr.sh

--- cidr.sh
+++ cidr.sh
@@ -13,12 +13,33 @@ cexit() {
 
 trap 'cexit 1' SIGINT;
 
-if [ -z "$1" ] || [ "$#" -ne 1 ]; then
-    echo -e "${GREEN}Usage: bash $0 <input_file>${NC}"
+default_ports="66,80,81,443,445,457,1080,1100,1241,1352,1433,1434,1521,1944,2301,3000,3128,3306,4000,4001,4002,4100,5000,5001,5432,5800,5801,5802,6346,6347,7001,7002,8080,8443,8888,30821" 
+
+while getopts "l:p:" opt; do
+    case $opt in
+        l)
+            input_file="$OPTARG"
+            ;;
+        p)
+            custom_ports="$OPTARG"
+            ;;
+        \?)
+            echo -e "${GREEN}Usage: bash $0 -l <input_file> [-p<ports>]${NC}"
+            cexit 1
+            ;;
+    esac
+done
+
+if [ -z "$input_file" ]; then
+    echo -e "${GREEN}Usage: bash $0 -l <input_file> [-p<ports>]${NC}"
     cexit 1
 fi
 
-input_file="$1"
+if [ -z "$custom_ports" ]; then
+    ports_option="-p$default_ports"
+else
+    ports_option="-p$custom_ports"
+fi
 
 if [ ! -f "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is not present in the directory.${NC}"
@@ -29,22 +50,23 @@ if [ ! -s "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is empty.${NC}"
     cexit 1
 fi
+
 if [ ! -r "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is not readable.${NC}"
     cexit 1
 fi
 
-echo -e "${YELLOW}[!] Finding open ports on all the ip's.${NC}";
-sudo masscan -iL "$input_file" -p1-65535 --rate=10000000 | anew masscan.txt > /dev/null || cexit $?;
-echo -e "${GREEN}[*] Result is stored to masscan.txt${NC}";
-echo -e "${GREEN}[**]" $(cat masscan.txt | wc -l)" ip's found with open ports.${NC}";
+echo -e "${YELLOW}[!] Finding open ports on all the IP's.${NC}"
+sudo masscan -iL "$input_file" "$ports_option" --rate=10000000 | anew masscan.txt > /dev/null || cexit $?;
+echo -e "${GREEN}[*] Result is stored to masscan.txt${NC}"
+echo -e "${GREEN}[**]" $(cat masscan.txt | wc -l)" IP's found with open ports.${NC}"
 cat masscan.txt | cut -d " " -f6 | sed 's/n//g' | sed 's/^ *\|\ *$//g' | sed 's/$/:/' | tee -a ip_add.txt > /dev/null || cexit $?;
-cat masscan.txt | cut -d "/" -f1 | tee -a port.txt > /dev/null || cexit $?;
+cat masscan.txt | cut -d "/" -f1 | cut -d 't' -f2 | sed 's/^ *//g' | tee -a port.txt > /dev/null || cexit $?;
 
-echo -e "${YELLOW}[!] httpx is running on all the ip's found with respective ports.${NC}";
+echo -e "${YELLOW}[!] httpx is running on all the IP's found with respective ports.${NC}"
 paste ip_add.txt port.txt | sed 's/\t//g' | httpx -silent -sc -td -cl | tee -a httpx_ip.txt || cexit $?;
 
-rm ip_add.txt port.txt;
-echo -e "${GREEN}[*] httpx result is stored in httpx_ip.txt${NC}";
-echo -e "${GREEN}[**]" $(cat httpx_ip.txt | wc -l)" ip's giving any response on browser.${NC}";
+rm ip_add.txt port.txt masscan.txt;
+echo -e "${GREEN}[*] httpx result is stored in httpx_ip.txt${NC}"
+echo -e "${GREEN}[**]" $(cat httpx_ip.txt | wc -l)" IP's giving any response on the browser.${NC}";
 echo "....................SCRIPT ENDED......................"
