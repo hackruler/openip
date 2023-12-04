e894082ed8e7d5347157e9be078decb485c6f4a8
h@ckrul3r <hackruler5770@gmail.com>
2023-12-05 01:58:48 +0530

    Update cidr.sh

--- cidr.sh
+++ cidr.sh
@@ -8,19 +8,18 @@ NC='\033[0m' # No Color
 
 
 show_help() {
-    echo ""
     echo "   Usage: bash cidr.sh -l <cidr file> [options]"
+    echo ""
     echo "   Options:"
     echo "   -h                          Help Menu"
     echo "   -l <input_file>             Specify the CIDR file you want to scan."
     echo "   -p[input_ports]             Specify the ports you want to scan (e.g., -p80,443 or -p1-65535)."
     echo "                               If not Specified anyt port, It will scan for default ports."
     echo "   -o <output_file>            Specify the output file."
-    echo "   -s <httpx probes>           Specify httpx probes you want to use (e.g., -s \"-sc -cl -title -td\")"
-    echo "                               If not specified it will use some default probes"
 
     exit 0
 }
+
 cexit() {
     echo -e "${RED}[!] Script interrupted. Exiting...${NC}"
     [ -e ip_add.txt ] && rm -f ip_add.txt
@@ -34,7 +33,7 @@ default_ports="66,80,81,443,445,457,1080,1100,1241,1352,1433,1434,1521,1944,2301
 
 default_probes="-sc -cl -td -title"
 
-while getopts ":l:p:o:s:h" opt; do
+while getopts ":l:p:o:h" opt; do
     case $opt in
         l)
             input_file="$OPTARG"
@@ -43,10 +42,7 @@ while getopts ":l:p:o:s:h" opt; do
             custom_ports="$OPTARG"
             ;; 
         o)
-            output_file="$OPTARG"
-            ;;
-        s)
-            httpx_probes="${OPTARG}" 
+            output_file="$OPTARG" 
             ;;
         h)
             show_help
@@ -62,17 +58,8 @@ if [ -z "$input_file" ]; then
     exit 0
 fi
 
-if [ -z "$custom_ports" ]; then
-    ports_option="-p$default_ports"
-else
-    ports_option="-p$custom_ports"
-fi
 
-if [ -z "$httpx_probes" ]; then
-    probes_option="$default_probes"
-else
-    probes_option="$httpx_probes"
-fi
+{ [ -z "$output_file" ] && ports_option="-p$default_ports" || ports_option="-p$custom_ports"; }
 
 if [ ! -f "$input_file" ]; then
     echo -e "${RED}Error: The specified input file '$input_file' is not present in the directory.${NC}"
@@ -95,17 +82,15 @@ echo -e "${GREEN}[**]" $(cat masscan.txt | wc -l)" IP's found with open ports.${
 cat masscan.txt | cut -d " " -f6 | sed 's/n//g' | sed 's/^ *\|\ *$//g' | sed 's/$/:/' | tee -a ip_add.txt > /dev/null;
 cat masscan.txt | cut -d "/" -f1 | cut -d 't' -f2 | sed 's/^ *//g' | tee -a port.txt > /dev/null;
 
-echo -e "${YELLOW}[!] httpx is running on all the IP's found with respective ports.${NC}"
-if [ -z "$output_file" ]; then
-    paste ip_add.txt port.txt | sed 's/\t//g' | httpx -silent $probes_option
-else
-    paste ip_add.txt port.txt | sed 's/\t//g' | httpx -silent $probes_option | tee -a "$output_file"
-fi
+echo -e "${YELLOW}[!] Combining ip's with their respective ports.${NC}"
+
+
+{ [ -z "$output_file" ] && paste ip_add.txt port.txt | sed 's/\t//g' || paste ip_add.txt port.txt | sed 's/\t//g' | tee -a "$output_file" > /dev/null; }
 
 rm ip_add.txt port.txt;
-if [ -z "$output_file" ]; then
-    echo -e "${GREEN}[*] ........... SCRIPT ENDED .................${NC}"
-else
-    echo -e "${GREEN}[*] httpx result is stored in "$output_file"${NC}"
-    echo -e "${GREEN}[**]" $(cat "$output_file" | wc -l)" IP's giving any response on the browser.${NC}";
-fi
+
+{
+    [ -z "$output_file" ] && 
+    echo -e "${GREEN}[*] ........... SCRIPT ENDED .................${NC}" || 
+    echo -e "${GREEN}[**]" $(cat "$output_file" | wc -l)" IP's .${NC}"; 
+}
