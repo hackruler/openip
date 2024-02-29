5032eb0db8659d810cbf3e4832e87dd89a9fd4c4
h@ckrul3r <hackruler5770@gmail.com>
2024-02-29 13:52:18 +0530

    Update openip.sh
    added rate as a input

--- openip.sh
+++ openip.sh
@@ -10,10 +10,11 @@ show_help() {
     echo ""
     echo "   Options:"
     echo "   -h                          Help Menu"
-    echo "   -l <input_file>             Specify the CIDR file you want to scan."
-    echo "   -p <input_ports>            Specify the ports you want to scan (e.g., -p80,443 or -p1-65535)."
+    echo "   -l <input file>             Specify the CIDR file you want to scan."
+    echo "   -p <input ports>            Specify the ports you want to scan (e.g., -p80,443 or -p1-65535)."
     echo "                               If not specified, it will scan for default ports."
-    echo "   -o <output_file>            Specify the output file."
+    echo "   -r <define rate>            Rate for scanning (Default rate = 100)"
+    echo "   -o <output file>            Specify the output file."
 
     exit 0
 }
@@ -25,9 +26,11 @@ cexit() {
 
 trap 'cexit 1' SIGINT;
 
+default_rate="100"
+
 default_ports="66,80,81,443,445,457,1080,1100,1241,1352,1433,1434,1521,1944,2301,3000,3128,3306,4000,4001,4002,4100,5000,5001,5432,5800,5801,5802,6346,6347,7001,7002,8080,8443,8888,30821" 
 
-while getopts ":l:p:o:h" opt; do
+while getopts ":l:p:o:r:h" opt; do
     case $opt in
         l)
             input_file="$OPTARG"
@@ -37,6 +40,9 @@ while getopts ":l:p:o:h" opt; do
             ;;
         o)
             output_file="$OPTARG" 
+            ;;
+        r) 
+            rate_input="$OPTARG"
             ;;
         h)
             show_help
@@ -57,6 +63,12 @@ if [ -z "$input_file" ]; then
         show_help
     fi
 fi
+
+{ 
+    [ -z "$rate_input" ] && 
+    custom_rate="$default_rate" || 
+    custom_rate="$rate_input"; 
+}
 
 { 
     [ -z "$custom_ports" ] && 
@@ -82,7 +94,7 @@ fi
 
 echo -e "${YELLOW}[!] Finding open ports on all the IP's.${NC}"
 
-sudo masscan -iL "$input_file" "$ports_option" --rate=10000000 | awk '{print $6 ":" $4}' | sed 's/\/tcp//g' | { [ -z "$output_file" ] && cat || tee "$output_file" > /dev/null; };
+sudo masscan -iL "$input_file" "$ports_option" --rate="$custom_rate" | awk '{print $6 ":" $4}' | sed 's/\/tcp//g' | { [ -z "$output_file" ] && cat || tee "$output_file" > /dev/null; };
 
 {
     [ -z "$output_file" ] && 
